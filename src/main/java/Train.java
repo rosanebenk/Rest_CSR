4e68fdbcbef7cf692a48460eb54affeb7c3d12da
huthomas <hugo.thomas@etudiant.univ-rennes1.fr>
2022-11-23 17:23:29 +0100

    Debut classe Train & Voyageur

--- src/main/java/Train.java
+++ src/main/java/Train.java
@@ -1,2 +1,15 @@
-public class Train {
+public class Train extends Thread{
+    private static int NB_PLACE_LIBRE;
+    private static int ARRET_TRAIN;
+    private static int VITESSE_TRAIN;
+
+    public boolean quaiLibre;
+
+    public Train(){
+
+    }
+
+    public void run(){
+
+    }
 }
--- src/main/java/Voyageur.java
+++ src/main/java/Voyageur.java
@@ -1,2 +1,21 @@
-public class Voyageur {
+public class Voyageur extends Thread{
+
+    private int billet;
+    public Voyageur(int billet){
+        this.billet = billet;
+    }
+
+    public void AcheterBillet(){
+
+    }
+
+    public void monterTrain(){
+
+    }
+
+    public void run(){
+        //Appel des fonctions
+        // acheter billet
+        // monter train
+    }
 }
