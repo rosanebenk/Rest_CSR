6a35128a19e29852865ba300bb1140fdc79efe6f
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 17:46:09 +0100

    espace Vente

--- src/main/java/EspaceVente.java
+++ src/main/java/EspaceVente.java
@@ -1,18 +1,29 @@
 import static java.lang.Thread.sleep;
 
 public class EspaceVente extends Thread{
-    private int billets;
+    public int billets;
+    private final int BILLET_MAX = 50;
 
-    public EspaceVente(int billets) {
+    public EspaceVente(int billets) throws Exception {
+        if (billets < BILLET_MAX)
         this.billets = billets;
+        else throw new Exception("Le nombre de billet max est 50");
     }
     public void vendre(int n, Voyageur v ){
+        while (billets == 0){
+            try{
+                this.wait();
+            }catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
         billets =- n;
         //v
     }
-    public void imprimer(int n) throws InterruptedException {
+    public void imprimer() throws InterruptedException {
         Thread.sleep(500);
-        billets+= n;
+        billets+= BILLET_MAX - billets;
     }
     public void run(){
 
