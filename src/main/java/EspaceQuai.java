9a57687ec0a1ad04b2edf48d6daa6ba30c931c9e
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 18:10:06 +0100

    Espace Quai

--- src/main/java/EspaceQuai.java
+++ src/main/java/EspaceQuai.java
@@ -1,11 +1,16 @@
 public class EspaceQuai {
-    private final int voies = 5 ;
-    private boolean train;
+    public final int VOIES = 2 ;
+    public int nbtrain;
 
-    public EspaceQuai(boolean train) {
-        this.train = false;
+
+    public EspaceQuai() {
+        this.nbtrain = 0;
     }
     public void monter(Voyageur v){
 
+    }
+    public synchronized boolean voieLibre(){
+        if (nbtrain == VOIES) return false;
+        else return true;
     }
 }
