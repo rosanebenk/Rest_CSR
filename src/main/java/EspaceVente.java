ec52b961de9a4e4a6aa46018fefd5c1ae1a9d1d4
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 17:56:32 +0100

    vente billet

--- src/main/java/EspaceVente.java
+++ src/main/java/EspaceVente.java
@@ -9,7 +9,7 @@ public class EspaceVente extends Thread{
         this.billets = billets;
         else throw new Exception("Le nombre de billet max est 50");
     }
-    public void vendre(int n, Voyageur v ){
+    public void vendre( ){
         while (billets == 0){
             try{
                 this.wait();
@@ -18,8 +18,9 @@ public class EspaceVente extends Thread{
                 e.printStackTrace();
             }
         }
-        billets =- n;
-        //v
+        billets --;
+        notifyAll();
+        System.out.println("Billet vendu");
     }
     public void imprimer() throws InterruptedException {
         Thread.sleep(500);
