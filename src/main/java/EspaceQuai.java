0e9aecd8a21d96a96ba4a902a7d7e66abefc6896
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 18:18:14 +0100

    nb train dans Espace Quai

--- src/main/java/EspaceQuai.java
+++ src/main/java/EspaceQuai.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
+
 public class EspaceQuai {
     public final int VOIES = 2 ;
     public int nbtrain;
-
+    public ArrayList<Train> trains;
 
     public EspaceQuai() {
         this.nbtrain = 0;
     }
     public void monter(Voyageur v){
-
+  // for train if place libre >0 supprimeplace et return place restant
     }
     public synchronized boolean voieLibre(){
         if (nbtrain == VOIES) return false;
