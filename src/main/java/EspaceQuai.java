788c42268737ad5aa31c42d97219226d8ec7f8a6
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 17:22:38 +0100

    EspaceQuai et EspaceVente

--- src/main/java/EspaceQuai.java
+++ src/main/java/EspaceQuai.java
@@ -1,2 +1,11 @@
 public class EspaceQuai {
+    private final int voies = 5 ;
+    private boolean train;
+
+    public EspaceQuai(boolean train) {
+        this.train = false;
+    }
+    public void monter(Voyageur v){
+
+    }
 }
--- src/main/java/EspaceVente.java
+++ src/main/java/EspaceVente.java
@@ -1,2 +1,20 @@
-public class EspaceVente {
+import static java.lang.Thread.sleep;
+
+public class EspaceVente extends Thread{
+    private int billets;
+
+    public EspaceVente(int billets) {
+        this.billets = billets;
+    }
+    public void vendre(int n, Voyageur v ){
+        billets =- n;
+        //v
+    }
+    public void imprimer(int n) throws InterruptedException {
+        Thread.sleep(500);
+        billets+= n;
+    }
+    public void run(){
+
+    }
 }
