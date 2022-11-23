6fcf64c9e1bf853440e6a3430532d20d44e20125
rosane6219 <im_benkhelil@esi.dz>
2022-11-23 17:47:37 +0100

    rectification nb billet

--- src/main/java/EspaceVente.java
+++ src/main/java/EspaceVente.java
@@ -5,7 +5,7 @@ public class EspaceVente extends Thread{
     private final int BILLET_MAX = 50;
 
     public EspaceVente(int billets) throws Exception {
-        if (billets < BILLET_MAX)
+        if (billets <= BILLET_MAX)
         this.billets = billets;
         else throw new Exception("Le nombre de billet max est 50");
     }
