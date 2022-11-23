32397f3652ad0a6d3e517a6080b36ad8f73a21a1
huthomas <hugo.thomas@etudiant.univ-rennes1.fr>
2022-11-23 18:10:21 +0100

    Modif classe Train

--- src/main/java/Train.java
+++ src/main/java/Train.java
@@ -1,12 +1,28 @@
 public class Train extends Thread{
-    private static int NB_PLACE_LIBRE;
-    private static int ARRET_TRAIN;
-    private static int VITESSE_TRAIN;
+    private static int NB_PLACE_LIBRE = 25;
+    private static int ARRET_TRAIN = 100;
+    private static int VITESSE_TRAIN = 250;
 
-    public boolean quaiLibre;
+    public boolean voieLibre;
+    public int nbPlaceLibre;
 
-    public Train(){
+    public Train(int nbPlaceLibre){
+        this.nbPlaceLibre = NB_PLACE_LIBRE;
+    }
 
+    public void arriver(EspaceQuai espaceQuai){
+        while (espaceQuai.voieLibre()){
+            try {
+                sleep(10000/VITESSE_TRAIN);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void depart(EspaceQuai espaceQuai){
+        espaceQuai.nbtrain --;
     }
 
     public void run(){
