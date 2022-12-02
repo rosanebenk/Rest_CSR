02032f80216e8e340bfb62495cc1f62c36a5aaba
huthomas <hugo.thomas@etudiant.univ-rennes1.fr>
2022-12-02 15:43:24 +0100

    Ajout InMemoryDatabase et VoyageurRessources

--- src/main/java/database/InMemoryDatabase.java
+++ src/main/java/database/InMemoryDatabase.java
@@ -1,5 +1,87 @@
 package database;
 
+import internals.EspaceQuai;
+import internals.EspaceVente;
+import internals.Train;
+import internals.Voyageur;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryDatabase {
+
+    /** User count (next id to give).*/
+    private int voyageurCount_;
+    private int trainCount_;
+
+    /** User Hashmap. */
+    Map<Integer, Voyageur> voyageurs_;
+    Map<Integer, Train> train_;
+
+
+    public InMemoryDatabase()
+    {
+        voyageurs_ = new HashMap<Integer, Voyageur>();
+        train_ = new HashMap<Integer, Train>();
+    }
+
+    /**
+     *
+     * Synchronized user creation.
+     *
+     *
+     *
+     * @return the user created
+     */
+    public synchronized Voyageur createVoyageur()
+    {
+        Voyageur user = new Voyageur(new EspaceVente(), new EspaceQuai());
+        user.setId(voyageurCount_);
+        voyageurs_.put(voyageurCount_, user);
+        voyageurCount_++;
+        return user;
+    }
+
+
+    public synchronized Train createTrain(Train train)
+    {
+        //Train train = new Train();
+        train.setId(trainCount_);
+        train_.put(trainCount_, train);
+        voyageurCount_++;
+        return train;
+    }
+
+    public Collection<Voyageur> getVoyageurs()
+    {
+        return voyageurs_.values();
+    }
+
+
+    public Collection<Train> getTrains()
+    {
+        return train_.values();
+    }
+
+
+
+    public Voyageur getVoyageur(int id)
+    {
+        return voyageurs_.get(id);
+    }
+
+    public synchronized void DeleteVoyageur(int id){
+        voyageurs_.remove(id);
+    }
+
+    public Train getTrain(int id)
+    {
+        return train_.get(id);
+    }
+
+    public synchronized void DeleteTrain(int id){
+        train_.remove(id);
+    }
 
 }
--- src/main/java/ressources/VoyageursRessource.java
+++ src/main/java/ressources/VoyageursRessource.java
@@ -1,6 +1,7 @@
 package ressources;
 
 import database.InMemoryDatabase;
+import internals.Voyageur;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,4 +16,61 @@ import java.util.Collection;
 
 public class VoyageursRessource extends ServerResource {
 
+    /** Database. */
+    private InMemoryDatabase db_;
+
+    /**
+     * Constructor.
+     * Call for every single user request.
+     */
+    public VoyageursRessource()
+    {
+        super();
+        db_ = (InMemoryDatabase) getApplication().getContext().getAttributes()
+                .get("database");
+    }
+
+    /**
+     *
+     * Returns the list of all the users
+     *
+     * @return  JSON representation of the users
+     * @throws JSONException
+     */
+    @Get("json")
+    public Representation getVoyageurs() throws JSONException
+    {
+        Collection<Voyageur> voyageurs = db_.getVoyageurs();
+        Collection<JSONObject> jsonVoyageurs = new ArrayList<JSONObject>();
+
+        for (Voyageur voyageur : voyageurs)
+        {
+            JSONObject current = new JSONObject();
+            current.put("id", voyageur.getId());
+            current.put("url", getReference() + "/" + voyageur.getId());
+            jsonVoyageurs.add(current);
+
+        }
+        JSONArray jsonArray = new JSONArray(jsonVoyageurs);
+        return new JsonRepresentation(jsonArray);
+    }
+
+    @Post("json")
+    public Representation createVoyageur(JsonRepresentation representation)
+            throws Exception
+    {
+        JSONObject object = representation.getJsonObject();
+        String name = object.getString("name");
+        int age = object.getInt("age");
+
+        // Save the user
+        Voyageur voyageur = db_.createVoyageur();
+
+        // generate result
+        JSONObject resultObject = new JSONObject();
+        resultObject.put("id", voyageur.getId());
+
+        JsonRepresentation result = new JsonRepresentation(resultObject);
+        return result;
+    }
 }
