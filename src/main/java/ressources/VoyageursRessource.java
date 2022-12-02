c08fe6efa3ae7c1212befcfa9e4edc0ca5b5394e
huthomas <hugo.thomas@etudiant.univ-rennes1.fr>
2022-12-02 15:00:06 +0100

    Modif Pom.xml

--- src/main/java/ressources/VoyageursRessource.java
+++ src/main/java/ressources/VoyageursRessource.java
@@ -1,4 +1,18 @@
 package ressources;
 
-public class VoyageursRessource {
+import database.InMemoryDatabase;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class VoyageursRessource extends ServerResource {
+
 }
